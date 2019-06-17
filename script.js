750c5689ba5e6d14b1eeebf78f0855dfad0fc821
Théodior <Theotasticque@theos-mbp-2.home>
2019-06-17 21:43:39 +0200

    Kommentarer på css og JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-/*burger menu*/
+/*BURGERMENU JavaScript*/
+
+/*Når siden loader op, skal den kalde funtionen sidenVises*/
 window.addEventListener("load", sidenVises);
 
+
+/*Her vælger vi #menuknap id'et og giver den en clickeffekt, som fører os til functionen toggleMenu*/
 function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
+/*toggleMenu er vores tænd og sluk knap, der enten lader vores burgermenu være ☰ hvis den ikke er trykket,*/
+/*men hvis den er trykket er id'et #menu hidden og ☰ ændre sig til et X*/
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
